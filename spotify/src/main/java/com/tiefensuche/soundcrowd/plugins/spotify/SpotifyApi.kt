f9eb0a9acbf27004aeaed4ec851d392937a6a4ee
0xf4b1 <10889432+0xf4b1@users.noreply.github.com>
2024-10-05 18:13:06 +0200

    Move to spotify-kt

--- spotify/src/main/java/com/tiefensuche/soundcrowd/plugins/spotify/SpotifyApi.kt
+++ spotify/src/main/java/com/tiefensuche/soundcrowd/plugins/spotify/SpotifyApi.kt
@@ -9,8 +9,9 @@ import android.support.v4.media.MediaMetadataCompat
 import android.support.v4.media.RatingCompat
 import com.tiefensuche.soundcrowd.extensions.MediaMetadataCompatExt
 import com.tiefensuche.soundcrowd.extensions.WebRequests
-import org.json.JSONArray
-import org.json.JSONObject
+import com.tiefensuche.spotify.api.Playlist
+import com.tiefensuche.spotify.api.SpotifyApi
+import com.tiefensuche.spotify.api.Track
 import xyz.gianlu.librespot.audio.decoders.AudioQuality
 import xyz.gianlu.librespot.audio.decoders.VorbisOnlyAudioQuality
 import xyz.gianlu.librespot.core.OAuth
@@ -19,15 +20,13 @@ import xyz.gianlu.librespot.mercury.MercuryRequests.KEYMASTER_CLIENT_ID
 import xyz.gianlu.librespot.metadata.TrackId
 import xyz.gianlu.librespot.player.decoders.SeekableInputStream
 import java.io.File
-import java.util.*
 
 class SpotifyApi(private val appContext: Context, private val context: Context) {
 
+    private lateinit var api: SpotifyApi
     private var session: Session? = null
     internal val credentialsFile = File(appContext.filesDir.path + "/credentials.json")
     internal val oauth = OAuth(KEYMASTER_CLIENT_ID, "soundcrowd://127.0.0.1/login")
-    private val nextQueryUrls: HashMap<String, String> = HashMap()
-    private var savedTrackIds: MutableSet<String> = HashSet()
 
     private fun createSession() {
         val conf = Session.Configuration.Builder()
@@ -46,6 +45,8 @@ class SpotifyApi(private val appContext: Context, private val context: Context) 
                 it.credentials(oauth.credentials)
             }
         }.create()
+
+        api = SpotifyApi(token())
     }
 
     private fun token(): String {
@@ -55,139 +56,68 @@ class SpotifyApi(private val appContext: Context, private val context: Context) 
     }
 
     fun getReleaseRadar(refresh: Boolean): List<MediaMetadataCompat> {
-        val response = JSONObject(request(RELEASE_RADAR_URL, "GET").value)
-        val id = response.getJSONObject("playlists").getJSONArray("items").getJSONObject(0).getString("id")
-        val items = request(String.format(PLAYLIST_URL, id), refresh)
-        return parseTracks(items)
+        return parseTracks(api.getReleaseRadar(refresh))
     }
 
     fun getUsersSavedTracks(refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(USERS_SAVED_TRACKS_URL, refresh)
-        return parseTracks(items)
+        return parseTracks(api.getUsersSavedTracks(refresh))
     }
 
     fun getArtists(refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(USERS_FOLLOWING, refresh, "artists")
-        return parsePlaylists(items)
+        return parsePlaylists(api.getArtists(refresh))
     }
 
     fun getArtist(id: String, refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(ARTIST_TRACKS.format(id), refresh, "tracks")
-        return parseTracks(items)
+        return parseTracks(api.getArtist(id, refresh))
     }
 
     fun getUsersPlaylists(refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(USERS_PLAYLISTS, refresh)
-        return parsePlaylists(items)
+        return parsePlaylists(api.getUsersPlaylists(refresh))
     }
 
     fun getPlaylist(id: String, refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(String.format(PLAYLIST_URL, id), refresh)
-        return parseTracks(items)
+        return parseTracks(api.getPlaylist(id, refresh))
     }
 
     fun query(query: String, refresh: Boolean): List<MediaMetadataCompat> {
-        val items = request(String.format(QUERY_URL, query), refresh, "tracks")
-        return parseTracks(items)
+        return parseTracks(api.query(query, refresh))
     }
 
-    private fun request(url: String, refresh: Boolean, type: String? = null): JSONArray {
-        val request = if (!refresh && url in nextQueryUrls) {
-            if (nextQueryUrls[url] == "null") {
-                return JSONArray()
-            }
-            nextQueryUrls[url].toString()
-        } else {
-            url
-        }
-        var response = JSONObject(request(request, "GET").value)
-        if (type != null && response.get(type) is JSONObject) {
-            response = response.getJSONObject(type)
-        }
-        nextQueryUrls[url] = if (response.has("next")) response.getString("next") else "null"
-        if (type != null && response.has(type))
-            return response.getJSONArray(type)
-        return response.getJSONArray("items")
-    }
-
-    private fun parseTracks(items: JSONArray): List<MediaMetadataCompat> {
+    private fun parseTracks(tracks: List<Track>): List<MediaMetadataCompat> {
         val result = mutableListOf<MediaMetadataCompat>()
-        if (items.length() == 0) {
+        if (tracks.isEmpty()) {
             return result
         }
 
-        val savedStatus = getTracksSavedStatus(items)
-        for (i in 0 until items.length()) {
-            var item = items.getJSONObject(i)
-            if (item.has("track")) {
-                item = item.getJSONObject("track")
-            }
-
-            result.add(MediaMetadataCompat.Builder()
-                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, item.getString("uri"))
-                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, item.getString("name"))
-                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, item.getJSONArray("artists").getJSONObject(0).getString("name"))
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, item.getJSONObject("album").getString("name"))
-                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, item.getJSONObject("album").getJSONArray("images").getJSONObject(0).getString("url"))
-                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI, item.getString("uri"))
-                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, item.getLong("duration_ms"))
+        return tracks.map {
+            MediaMetadataCompat.Builder()
+                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, it.id)
+                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, it.title)
+                .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, it.artist)
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM, it.album)
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, it.artwork)
+                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_URI, it.url)
+                .putLong(MediaMetadataCompat.METADATA_KEY_DURATION, it.duration)
+                .putRating(MediaMetadataCompatExt.METADATA_KEY_FAVORITE, RatingCompat.newHeartRating(it.liked))
                 .putString(MediaMetadataCompatExt.METADATA_KEY_TYPE, MediaMetadataCompatExt.MediaType.MEDIA.name)
-                .putRating(MediaMetadataCompatExt.METADATA_KEY_FAVORITE, RatingCompat.newHeartRating(savedStatus.getBoolean(i)))
-                .build())
-
-            if (savedStatus.getBoolean(i)) {
-                savedTrackIds.add(item.getString("uri"))
-            }
+                .build()
         }
-        return result
     }
 
-    private fun parsePlaylists(items: JSONArray): List<MediaMetadataCompat> {
-        val result = mutableListOf<MediaMetadataCompat>()
-        for (i in 0 until items.length()) {
-            val item = items.getJSONObject(i)
-            result.add(MediaMetadataCompat.Builder()
-                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, item.getString("id"))
-                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, item.getString("name"))
+    private fun parsePlaylists(playlists: List<Playlist>): List<MediaMetadataCompat> {
+        return playlists.map {
+            MediaMetadataCompat.Builder()
+                .putString(MediaMetadataCompat.METADATA_KEY_MEDIA_ID, it.uuid)
+                .putString(MediaMetadataCompat.METADATA_KEY_TITLE, it.title)
                 .putString(MediaMetadataCompat.METADATA_KEY_ARTIST, "")
-                .putString(
-                    MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI,
-                    if (item.isNull("images") || item.getJSONArray("images").length() == 0) ""
-                    else item.getJSONArray("images").getJSONObject(0).getString("url")
-                )
+                .putString(MediaMetadataCompat.METADATA_KEY_ALBUM_ART_URI, it.artwork)
                 .putString(MediaMetadataCompatExt.METADATA_KEY_TYPE, MediaMetadataCompatExt.MediaType.STREAM.name)
                 .build()
-            )
         }
-        return result
-    }
-
-    private fun getTracksSavedStatus(items: JSONArray): JSONArray {
-        val ids = mutableListOf<String>()
-        for (i in 0 until items.length()) {
-            var item = items.getJSONObject(i)
-            if (item.has("track")) {
-                item = item.getJSONObject("track")
-            }
-            ids.add(item.getString("id"))
-        }
-        return JSONArray(request("$USERS_SAVED_TRACKS_URL/contains?ids=${ids.joinToString(",")}", "GET").value)
     }
 
     fun saveTrack(uri: String): Boolean {
-        return try {
-            val exists = savedTrackIds.contains(uri)
-            request("$USERS_SAVED_TRACKS_URL?ids=${uri.substringAfterLast(':')}",
-                if (exists) "DELETE" else "PUT")
-            if (exists) {
-                savedTrackIds.remove(uri)
-            } else {
-                savedTrackIds.add(uri)
-            }
-            true
-        } catch (e: WebRequests.HttpException) {
-            false
-        }
+        return api.saveTrack(uri)
     }
 
     fun streamUri(uri: String): MediaDataSource {
@@ -227,16 +157,5 @@ class SpotifyApi(private val appContext: Context, private val context: Context) 
         override fun getSize(): Long {
             return stream.size().toLong() - pos
         }
-    }
-
-    companion object {
-        private const val BASE_URL = "https://api.spotify.com/v1"
-        private const val QUERY_URL = "$BASE_URL/search?q=%s&type=track"
-        private const val USERS_SAVED_TRACKS_URL = "$BASE_URL/me/tracks"
-        private const val USERS_FOLLOWING = "$BASE_URL/me/following?type=artist"
-        private const val ARTIST_TRACKS = "$BASE_URL/artists/%s/top-tracks?market=US"
-        private const val USERS_PLAYLISTS = "$BASE_URL/me/playlists"
-        private const val RELEASE_RADAR_URL = "$BASE_URL/search?q=Release-Radar&type=playlist&limit=1"
-        private const val PLAYLIST_URL = "$BASE_URL/playlists/%s/tracks?limit=50"
     }
 }
